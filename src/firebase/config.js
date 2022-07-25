bd922573f61a1d8ad57e0ade777ca24213dd4e4e
George Cristi <ashflakes@gmail.com>
2022-07-25 15:14:26 +0300

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDxtXt5Di6sMdRm7Bz6QBlYCi80gxC_Qn4",
+    authDomain: "memory-game-6c589.firebaseapp.com",
+    projectId: "memory-game-6c589",
+    storageBucket: "memory-game-6c589.appspot.com",
+    messagingSenderId: "47727641741",
+    appId: "1:47727641741:web:823bbf3391c0641015cec4"
+};
+
+firebase.initializeApp(firebaseConfig)
